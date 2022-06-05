159a4f716119ece6f35c3ac6921790aba6ed69d0
Daco <dacokim32@gmail.com>
2022-06-05 20:57:18 +0900

    backtesting

--- test.py
+++ test.py
@@ -12,3 +12,24 @@ fs.rsi(df, w=14) # w=14 > 일봉 기준 14일간의 주가의 상대적 세기
 fs.draw_chart(df, left='rsi', right='AAPL')
 
 # %%
+fs.indicator_to_signal(df, factor='rsi', buy=40, sell=60)
+# %%
+fs.position(df)
+fs.draw_chart(df, left='rsi', right='position_chart')
+# %%
+df # df에 어떤 컬럼이 들어있는지 확인 가능
+# %%
+fs.evaluate(df, cost=.001)
+fs.draw_chart(df, left='acc_rtn_dp', right='AAPL')
+df
+# %%
+fs.performance(df, rf_rate=0.01)
+# %%
+# buy=60, sell=40 전략으로 매매했다면?
+fs.indicator_to_signal(df, factor='rsi', buy=60, sell=40)
+fs.position(df)
+fs.evaluate(df, cost=.001)
+fs.performance(df, rf_rate=0.01)
+fs.draw_chart(df, left='rsi', right='position_chart')
+fs.draw_chart(df, left='acc_rtn_dp', right='AAPL')
+# %%
